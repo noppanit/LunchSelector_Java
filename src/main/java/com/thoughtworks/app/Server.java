95f86cab7db799424cc083c427fec48b4c4b2ca9
Noppanit Charassinvichai <ncharass@thoughtworks.com>
2011-05-23 11:47:37 +0100

    Initial Revision

---
+++ src/main/java/com/thoughtworks/app/Server.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+    }
+}
---
+++ src/test/java/com/thoughtworks/app/ServerTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.app;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Server.
+ */
+public class ServerTest
+{
+    @Test
+    public void test()
+    {
+
+    }
+}
