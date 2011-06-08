5b96d658dd4a30baaf0bc1dbe5ddae7bf2f9fe25
Noppanit Charassinvichai <ncharass@thoughtworks.com>
2011-06-08 11:36:49 +0100

    [ToyC, BrianS] Added tests for customer

--- src/test/java/com/thoughtworks/repository/CustomerTest.java
+++ src/test/java/com/thoughtworks/repository/CustomerTest.java
@@ -5,7 +5,9 @@ import com.thoughtworks.database.DatabaseHelper;
 import com.thoughtworks.model.Customer;
 import com.thoughtworks.model.Menu;
 import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 
 import java.text.ParseException;
 import java.util.Calendar;
@@ -73,5 +75,26 @@ public class CustomerTest extends BaseTest {
 
         assertThat(age, is(10));
     }
+
+    @Test
+    public void shouldReturnChildNodeForAgeUnderChildThreshold() {
+        DatabaseHelper db = DatabaseHelper.getInstance();
+        GraphDatabaseService graphDb = db.getDatabaseService();
+
+        Transaction transaction = graphDb.beginTx();
+        try {
+            CustomerRepository customerRepository = new CustomerRepository();
+            customerRepository.createCustomer("Bob","01/01/2000");
+            Node bob = customerRepository.getCustomer("Bob");
+
+            RuleRepository ruleRepository = new RuleRepository();
+            Node child = ruleRepository.evaluateRuleBasedOn("Age").withValue(customerRepository.getAge(bob));
+
+            assertThat(child.getProperty(DatabaseHelper.NODE_NAME).toString(), is("Child"));
+        } finally {
+            transaction.failure();
+        }
+    }
+
 
 }
